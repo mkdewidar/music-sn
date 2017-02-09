13c6a0c799d7604c34235b4182c17aaaf24ee334
Mohamed Dewidar <mkdewidar@gmail.com>
2017-02-09 12:29:40 +0000

    Add basic code to connect client and server asynch

--- Social Music Network/Client/src/musicss/client/Main.java
+++ Social Music Network/Client/src/musicss/client/Main.java
@@ -1,10 +1,26 @@
 package musicss.client;
 
+import java.io.IOException;
+
 /**
- * Program starts here.
+ * Program starts here, any network exceptions that
+ * result in the program not working are handled here.
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("The client is alive!");
+        ClientInstance instance;
+        try {
+            instance = new ClientInstance();
+        } catch (IOException e) {
+            System.out.println("ERROR " + e.getMessage());
+
+            return;
+        }
+
+        try {
+            instance.Start();
+        } catch (IOException e) {
+            System.out.println("ERROR " + e.getMessage());
+        }
     }
 }
--- Social Music Network/Server/src/musicss/server/Main.java
+++ Social Music Network/Server/src/musicss/server/Main.java
@@ -1,10 +1,36 @@
 package musicss.server;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * Program starts here.
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("The server is alive!");
+        ServerSocket serverSocket;
+        boolean isRunning = true;
+
+        try {
+            serverSocket = new ServerSocket(9090);
+        } catch (IOException e) {
+            System.out.println("ERROR " + e.getMessage());
+
+            return;
+        }
+
+        while (isRunning) {
+            try {
+                System.out.println("Awaiting client connection...");
+                Socket userSocket = serverSocket.accept();
+                System.out.println("Connected to user: " + userSocket.getInetAddress().getHostAddress());
+
+            } catch (IOException e) {
+                System.out.println("ERROR " + e.getMessage());
+            }
+
+        }
+
     }
 }
