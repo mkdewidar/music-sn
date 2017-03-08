19b9ff53293800af640570713019a755de884ee9
Mohamed Dewidar <mkdewidar@gmail.com>
2017-03-08 14:44:53 +0000

    Create login fxml with controller class

---
+++ Social Music Network/Client/src/musicss/client/LoginController.java
@@ -0,0 +1,50 @@
+package musicss.client;
+
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Controls the login screen of the interface.
+ */
+public class LoginController implements Initializable {
+    @FXML
+    private TextField usernameTextField;
+    @FXML
+    private PasswordField passwordTextField;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        usernameTextField.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                if (event.getCode() == KeyCode.ENTER)
+                {
+                    passwordTextField.requestFocus();
+                }
+            }
+        });
+        passwordTextField.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                if (event.getCode() == KeyCode.ENTER)
+                {
+                    System.out.println("Sending username: " + usernameTextField.getText()
+                            + ", password: "+ passwordTextField.getText());
+
+                    usernameTextField.clear();
+                    passwordTextField.clear();
+                }
+            }
+        });
+
+        System.out.println("The login screen has been initialised");
+    }
+}
